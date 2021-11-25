5897625771155a495726319ce31c22373d937dd1
Yevhenii Bezeha <yevhenii.bezeha@intellias.com>
2021-11-25 12:52:41 +0200

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,48 +1,22 @@
-import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [
-      {
-        path: ':id/:name',
-        component: UserComponent,
-      },
-    ],
-  },
-
-  {
-    path: 'servers',
-    component: ServersComponent,
-    children: [
-      {
-        path: ':id',
-        component: ServerComponent,
-      },
-      {
-        path: ':id/edit',
-        component: EditServerComponent,
-      },
-    ],
-  },
+  {path: '', component: HomeComponent},
+  {path: 'user/:id', component: UserComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+
+}
